a0474b33a9da67a0b7adc458b2d4d4b478e7b2fe
Peter Law <PeterJCLaw@gmail.com>
2018-03-11 19:16:09 +0000

    Import traits we're using

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
+use std::io::BufRead;
+use std::iter::FromIterator;
 use std::iter::Iterator;
 use std::path::Path;
 use std::vec::Vec;
