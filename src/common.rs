3462d7885b50cb0f295c32ae767914d1baee6d17
Peter Law <PeterJCLaw@gmail.com>
2018-03-11 19:47:11 +0000

    Switch to mutable structures
    This avoids the liftetime issues I was having, but results in
    structures which don't quite match up to the (mostly-immutable)
    ones in the Python implementation.

--- src/common.rs
+++ src/common.rs
@@ -5,6 +5,7 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::iter::FromIterator;
 use std::iter::Iterator;
+use std::path::PathBuf;
 use std::path::Path;
 use std::vec::Vec;
 use std::time::SystemTime;
@@ -15,74 +16,71 @@ use walkdir::WalkDir;
 const HASH_PREFIX_SIZE: usize = 1024 * 1024;
 
 #[derive(Debug, Eq, Hash, PartialEq)]
-pub struct ContentDescription<'a> {
+pub struct ContentDescription {
     size: u64,
-    hash: &'a str,
+    hash: String,
 }
 
 #[derive(Debug, Eq, Hash, PartialEq)]
-pub struct MovedFile<'a> {
-    old: &'a Path,
-    new: &'a Path,
+pub struct MovedFile {
+    old: PathBuf,
+    new: PathBuf,
 }
 
 #[derive(Debug, Eq, Hash, PartialEq)]
-pub struct CopiedFile<'a> {
-    old: &'a Path,
-    new: &'a Path,
+pub struct CopiedFile {
+    old: PathBuf,
+    new: PathBuf,
 }
 
 #[derive(Debug, Eq, Hash, PartialEq)]
-pub struct NewFile<'a> {
-    path: &'a Path,
+pub struct NewFile {
+    path: PathBuf,
 }
 
 #[derive(Debug, Eq, Hash, PartialEq)]
-pub struct ChangedFile<'a> {
-    path: &'a Path,
-    old_content: ContentDescription<'a>,
-    new_content: ContentDescription<'a>,
+pub struct ChangedFile {
+    path: PathBuf,
+    old_content: ContentDescription,
+    new_content: ContentDescription,
 }
 
 #[derive(Debug, Eq, Hash, PartialEq)]
-pub struct MissingFile<'a> {
-    path: &'a Path,
+pub struct MissingFile {
+    path: PathBuf,
 }
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct FileDescription<'a> {
+pub struct FileDescription {
     modified: SystemTime,
-    content: ContentDescription<'a>,
-    path: &'a Path,
+    content: ContentDescription,
+    path: PathBuf,
 }
 
 #[derive(Debug)]
-pub struct ChangeSummary<'a> {
-    changed: Vec<ChangedFile<'a>>,
-    copied: Vec<CopiedFile<'a>>,
-    moved: Vec<MovedFile<'a>>,
-    deleted: Vec<MissingFile<'a>>,
-    added: Vec<FileDescription<'a>>,
+pub struct ChangeSummary {
+    changed: Vec<ChangedFile>,
+    copied: Vec<CopiedFile>,
+    moved: Vec<MovedFile>,
+    deleted: Vec<MissingFile>,
+    added: Vec<FileDescription>,
 }
 
 
 #[derive(Debug, Eq, PartialEq)]
-pub enum MaybeFileDescription<'a> {
-    MissingFile(MissingFile<'a>),
-    FileDescription(FileDescription<'a>),
+pub enum MaybeFileDescription {
+    MissingFile(MissingFile),
+    FileDescription(FileDescription),
 }
 
 
-pub fn walk_files(directory: Path) -> Iterator<Item=Path> {
-    WalkDir::new(directory)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .map(|e| e.path())
+pub fn walk_files(directory: Path) -> Iterator<Item = PathBuf> {
+    WalkDir::new(directory).into_iter().filter_map(|e| e.ok())
 }
 
 
-pub fn hash_file(filepath: Path) -> str {
-    let f = File::open(filepath);
+pub fn hash_file(filepath: PathBuf) -> String {
+    let f = File::open(filepath).unwrap();
     let mut buffer = [0; HASH_PREFIX_SIZE];
     f.read_exact(&mut buffer);
     let digest = md5::compute(&buffer[..]);
@@ -90,7 +88,7 @@ pub fn hash_file(filepath: Path) -> str {
 }
 
 
-pub fn describe<'a>(filepath: Path) -> MaybeFileDescription<'a> {
+pub fn describe(filepath: PathBuf) -> MaybeFileDescription {
     let metadata_result = filepath.metadata();
     match metadata_result {
         Err(_) => MaybeFileDescription::MissingFile(MissingFile { path: filepath }),
@@ -107,13 +105,15 @@ pub fn describe<'a>(filepath: Path) -> MaybeFileDescription<'a> {
 
 
 pub fn path_by_content(
-    descriptions: Iterator<Item=FileDescription>,
-) -> HashMap<ContentDescription, Path> {
+    descriptions: &IntoIterator<Item = FileDescription, IntoIter = Iterator<Item = FileDescription>>,
+) -> HashMap<ContentDescription, PathBuf> {
     HashMap::from_iter(descriptions.map(|x| (x.content, x.path)))
 }
 
 
-pub fn load_descriptions<'a>(references: Iterator<Item=Path>) -> HashMap<Path, FileDescription<'a>> {
+pub fn load_descriptions(
+    references: &IntoIterator<Item = PathBuf, IntoIter = Iterator<Item = PathBuf>>,
+) -> HashMap<PathBuf, FileDescription> {
     HashMap::from_iter(
         references
             .flat_map(|path| {
