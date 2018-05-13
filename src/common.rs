b6e3eadafbfedcf3da870d7559ecec77a1c5ff39
Peter Law <PeterJCLaw@gmail.com>
2018-05-13 16:31:30 +0100

    Filter to only files

--- src/common.rs
+++ src/common.rs
@@ -113,6 +113,7 @@ where
     WalkDir::new(directory)
         .into_iter()
         .filter_map(|e| e.ok())
+        .filter(|e| !e.file_type().is_dir())
         .map(|e| e.path().to_path_buf())
         .collect()
 }
